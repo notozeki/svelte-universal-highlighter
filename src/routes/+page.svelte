bb9bc8200953eb12308c9dfc1f10fd607f632711
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2023-04-16 21:09:03 +0900

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,6 @@
 				<label class="block w-full mb-2 text-sm font-medium text-gray-900"
 					>highlighter
 					<select
-						on:select={() => (code = '')}
 						bind:value={highlighter}
 						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
 					>
