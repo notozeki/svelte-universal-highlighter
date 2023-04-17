01746b9767176b0b92bfa017687068b7b4bad39d
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2023-04-17 15:20:19 +0900

    :fix: レスポンシブのUI修正 - レスポンシブ時tabでcodeかpreviewを切り替えらるように - apikeyが空でもセットできるように

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 		type LanguageList,
 		type ThemeList
 	} from '$lib/highlight';
+	import { tick } from 'svelte';
 
 	const highlighters = getHighlighters();
 	let textArea: HTMLTextAreaElement;
@@ -18,8 +19,9 @@
 	let state: HighlightState = 'done';
 	let code = '';
 	let timerId: number;
-	let APIKey = '';
+	let APIKey: string | undefined;
 	let typingValue = '';
+	let tab: string = 'code';
 	$: {
 		initHighlighter(highlighter);
 	}
@@ -43,6 +45,16 @@
 		textArea.value = cursorLeft + '\t' + cursorRight;
 		textArea.selectionEnd = cursorPosition + 1;
 	};
+	const handleWriteCode = () => {
+		if (highlighter.name === 'GPT') {
+			clearInterval(timerId);
+			timerId = setTimeout(() => {
+				code = textArea.value;
+			}, 2000);
+			return;
+		}
+		code = textArea.value;
+	};
 </script>
 
 <div class={`w-full h-full ${state === 'loading' ? 'opacity-50' : 'opacity-100'}`}>
@@ -61,11 +73,11 @@
 					</select>
 				</label>
 				{#if highlighter.name === 'GPT'}
-					{#if !APIKey || state === 'error'}
+					{#if APIKey === undefined || state === 'error'}
 						<label for="highlighter" class="block mb-2 text-sm font-medium text-gray-900"
 							>apiKey
 						</label>
-						<div class="flex gap-2">
+						<div class="flex gap-2 w-[calc(100vw_-_100%)] sm:w-auto">
 							<input
 								type="password"
 								bind:value={typingValue}
@@ -111,40 +123,65 @@
 			</label>
 		</div>
 	</div>
-	<div class="flex justify-between w-full overflow-hidden box-border h-[calc(100vh_-_90px)]">
-		<div class="w-1/2 border-r-[1px] h-full border-x-black">
-			<div class="p-2 border-slate-300 border-b-[1px]">code</div>
-			<div class="h-full">
+	<div class="w-full overflow-hidden box-border">
+		<nav class="flex">
+			<button
+				on:click={() => {
+					tab = 'code';
+				}}
+				class={`sm:w-full sm:border-r-[1px] sm:border-b-[1px] sm:border-slate-300 sm:pointer-events-none hover:text-blue-500 ${
+					tab === 'code' && 'border-b-[2px] border-b-green-400'
+				}`}
+			>
+				<div class="p-2 border-slate-300 sm:border-b-[1px]">code</div>
+			</button>
+			<button
+				on:click={() => {
+					tab = 'preview';
+				}}
+				class={`sm:w-full sm:border-r-[1px] sm:border-b-[1px] sm:border-slate-300 sm:pointer-events-none hover:text-blue-500 ${
+					tab === 'preview' && 'border-b-[2px] border-b-green-400'
+				}`}
+			>
+				<div class="p-2 border-slate-300 sm:border-b-[1px] bg-white">Preview</div>
+			</button>
+		</nav>
+		<div
+			class="w-full flex flex-col sm:flex-row h-[calc(100vh_-_132px)] border-t-[1px] border-slate"
+		>
+			<div
+				class={`h-full w-full sm:w-1/2 border-r-[1px] border-x-black sm:inline-block  ${
+					tab === 'preview' && 'hidden'
+				}`}
+			>
 				<textarea
 					readonly={state === 'loading'}
 					bind:this={textArea}
 					on:focus={() => {
 						if (highlighter.name === 'GPT') {
-							if (!APIKey || state === 'error') {
+							if (APIKey === undefined || state === 'error') {
 								apiKeyInput.focus();
 							}
 						}
 					}}
 					on:keydown={(e) => {
 						onTabKey(e);
-						if (highlighter.name === 'GPT') {
-							clearInterval(timerId);
-							timerId = setTimeout(() => {
-								code = textArea.value;
-							}, 2000);
-							return;
-						}
-						code = textArea.value;
+					}}
+					on:paste={() => handleWriteCode()}
+					on:keyup={(e) => {
+						handleWriteCode();
 					}}
 					class="text-base outline-none box-border p-3 pt-4 bg-[#F6FAFD] resize-none w-full h-full"
 				/>
 			</div>
-		</div>
-		<div class="w-1/2 border-r-[1px] h-full border-x-black">
-			<div class="p-2 border-slate-300 border-b-[1px]">Preview</div>
-			<div class="w-full h-full px-2 text-base break-all">
+
+			<div
+				class={`h-full px-2 w-full sm:w-1/2 text-base break-all sm:inline-block ${
+					tab === 'code' && 'hidden'
+				}`}
+			>
 				<Highlight
-					extra={highlighter.name === 'GPT' ? { openaiKey: APIKey } : undefined}
+					extra={highlighter.name === 'GPT' ? { openaiKey: APIKey ? APIKey : '' } : undefined}
 					{theme}
 					{highlighter}
 					{language}
