7ceebbb9a9373e557964bd39bde35c8e07035363
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2023-04-17 22:56:26 +0900

    :feat: reviewコメントの修正
    - 濱口さんのメッセージを表示させるように
    apikeyがセットされる前とapikeyが空でセットされた場合はメッセージがのこるように
    
    - setを押すまでリクエストが飛ばないように
    - apikeyのラベル修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
 		type LanguageList,
 		type ThemeList
 	} from '$lib/highlight';
-	import { tick } from 'svelte';
 
 	const highlighters = getHighlighters();
 	let textArea: HTMLTextAreaElement;
@@ -19,9 +18,12 @@
 	let state: HighlightState = 'done';
 	let code = '';
 	let timerId: number;
-	let APIKey: string | undefined;
-	let typingValue = '';
 	let tab: string = 'code';
+	let openaiKey: string | undefined = undefined;
+	let extra: Record<string, string> | undefined;
+	const handleOpenaiKeySubmit = () => {
+		extra = { openaiKey: openaiKey ? openaiKey : '' };
+	};
 	$: {
 		initHighlighter(highlighter);
 	}
@@ -59,45 +61,19 @@
 
 <div class={`w-full h-full ${state === 'loading' ? 'opacity-50' : 'opacity-100'}`}>
 	<div class="p-3 border-b-[1px] border-slate-300">
-		<div class="grid grid-cols-3 gap-5 w-full">
-			<div>
-				<label class="block w-full mb-2 text-sm font-medium text-gray-900"
-					>highlighter
-					<select
-						bind:value={highlighter}
-						class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-					>
-						{#each highlighters as item}
-							<option value={item}>{item.name}</option>
-						{/each}
-					</select>
-				</label>
-				{#if highlighter.name === 'GPT'}
-					{#if APIKey === undefined || state === 'error'}
-						<label for="highlighter" class="block mb-2 text-sm font-medium text-gray-900"
-							>apiKey
-						</label>
-						<div class="flex gap-2 w-[calc(100vw_-_100%)] sm:w-auto">
-							<input
-								type="password"
-								bind:value={typingValue}
-								id="highlighter"
-								bind:this={apiKeyInput}
-								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-							/>
-							<button
-								class="bg-transparent hover:bg-blue-300 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-								on:click={() => {
-									APIKey = typingValue;
-								}}>Set</button
-							>
-						</div>
-					{/if}
-					{#if state === 'error'}
-						<p class="text-sm text-red-500">Invalid APIKey</p>
-					{/if}
-				{/if}
-			</div>
+		<div class="grid grid-cols-3 gap-x-5 w-full">
+			<label class="block w-full mb-2 text-sm font-medium text-gray-900"
+				>highlighter
+				<select
+					bind:value={highlighter}
+					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+				>
+					{#each highlighters as item}
+						<option value={item}>{item.name}</option>
+					{/each}
+				</select>
+			</label>
+
 			<label class="block mb-2 text-sm font-medium text-gray-900"
 				>language
 				<select
@@ -109,7 +85,6 @@
 					{/each}
 				</select>
 			</label>
-
 			<label class="block mb-2 text-sm font-medium text-gray-900"
 				>theme
 				<select
@@ -121,9 +96,43 @@
 					{/each}
 				</select>
 			</label>
+			<div class="col-span-3 lg:col-span-1 sm:col-span-2">
+				{#if highlighter.name === 'GPT'}
+					{#if extra === undefined || state === 'error'}
+						<label for="highlighter" class="block mb-2 text-sm font-medium text-gray-900"
+							>OpenAI API key
+						</label>
+						<form on:submit={handleOpenaiKeySubmit} class="flex gap-2 w-full sm:w-auto">
+							<input
+								placeholder="OpenAI API key"
+								type="password"
+								bind:value={openaiKey}
+								id="highlighter"
+								bind:this={apiKeyInput}
+								class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+							/>
+							<button
+								class="bg-transparent hover:bg-blue-300 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+								type="submit">Set</button
+							>
+						</form>
+					{/if}
+					{#if extra === undefined || extra?.openaiKey === ''}
+						<p class="w-full px-1 box-border col-span-3 md:whitespace-nowrap break-all">
+							Leave blank to try it with my API key. Keep in mind it will cost me, feel free to
+							donate if you find this fun :)
+							<br />
+							ETH 0xCAfdD9997a724687EbDB0AC7F15d8c8e6bc192F4
+						</p>
+					{/if}
+					{#if state === 'error'}
+						<p class="text-sm text-red-500">Invalid APIKey</p>
+					{/if}
+				{/if}
+			</div>
 		</div>
 	</div>
-	<div class="w-full overflow-hidden box-border">
+	<div class="w-full overflow-hidden h-full box-border">
 		<nav class="flex">
 			<button
 				on:click={() => {
@@ -147,7 +156,7 @@
 			</button>
 		</nav>
 		<div
-			class="w-full flex flex-col sm:flex-row h-[calc(100vh_-_132px)] border-t-[1px] border-slate"
+			class="w-full flex flex-col sm:flex-row border-t-[1px] border-slate h-[calc(100vh_-_132px)]"
 		>
 			<div
 				class={`h-full w-full sm:w-1/2 border-r-[1px] border-x-black sm:inline-block  ${
@@ -159,7 +168,7 @@
 					bind:this={textArea}
 					on:focus={() => {
 						if (highlighter.name === 'GPT') {
-							if (APIKey === undefined || state === 'error') {
+							if (openaiKey === undefined || state === 'error') {
 								apiKeyInput.focus();
 							}
 						}
@@ -181,7 +190,7 @@
 				}`}
 			>
 				<Highlight
-					extra={highlighter.name === 'GPT' ? { openaiKey: APIKey ? APIKey : '' } : undefined}
+					{extra}
 					{theme}
 					{highlighter}
 					{language}
